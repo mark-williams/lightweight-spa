3f4309f4bdd8ef5f0abca1ca09b9366d4e1958da
mark-williams <markwill@hotmail.co.uk>
2016-10-26 12:38:52 +0100

    Now shows requested content

--- assets/js/routing.js
+++ assets/js/routing.js
@@ -1,6 +1,6 @@
 var routing = function() {
     'use strict';
-    var DEFAULTTRANSITIONTIME = 300;
+    var DEFAULTTRANSITIONTIME = 100;
     return {
         init: initRouting,
     }
@@ -12,11 +12,20 @@ var routing = function() {
     }
 
     function onHashChange(event) {
-        console.log('HASH CHANGE', event);
-        // Show first page
-        var $pages = $('.page');
-        $pages.hide(0);
-        $pages.first().show(DEFAULTTRANSITIONTIME);   
+        render(window.location.hash);   
     }
     
+    function render(hash) {
+        var $pages = $('.page');
+        $pages.hide(0);
+
+        if (hash.length) {
+            var $pageToShow = $pages.filter(hash);
+            $pageToShow.show(DEFAULTTRANSITIONTIME);
+        }
+        else {
+            // Default to first page
+            $pages.first().show(DEFAULTTRANSITIONTIME);
+        }
+    }
 }();
